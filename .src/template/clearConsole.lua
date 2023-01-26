96a3b44360b1b26791a66ae0f76dd312086ac652
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-01-25 20:20:51 -0600

    template

---
+++ .src/template/clearConsole.lua
@@ -0,0 +1,14 @@
+-- print("If you would like to clear the console type 'clr'")
+
+-- local input = io.read()
+
+-- if input == "clr" then
+
+-- local function clearConsole()
+
+--     io.write("\27[2J")
+--     os.execute("cls")
+--     os.execute("clear")
+
+-- end
+-- clearConsole()
